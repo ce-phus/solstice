e133eb230483c00ca0465329d7e6be183ee7fe87
cephus <pnakitaren2@gmail.com>
2024-11-14 05:07:26 -0500

    task:Contact page email config

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
-import { contact } from '../assets'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { contact } from '../assets';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phonenumber: "",
+    message: "",
+    preferredContact: [],
+    urgency: ""
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      setForm((prevForm) => ({
+        ...prevForm,
+        preferredContact: checked
+          ? [...prevForm.preferredContact, value]
+          : prevForm.preferredContact.filter((contact) => contact !== value),
+      }));
+    } else if (type === "radio") {
+      setForm({ ...form, [name]: value });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      "service_agjupcw",
+      "template_pe5v36g",
+      {
+        from_firstname: form.firstname,
+        from_lastname: form.lastname,
+        from_phonenumber: form.phonenumber,
+        to_name: "Solstice Technologies Limited",
+        from_email: form.email,
+        to_email: "hello@solstice.co.ke",
+        message: form.message,
+        preferred_contact: form.preferredContact.join(", "),
+        urgency: form.urgency,
+      },
+      "5TKVQ_9rDJV810vPd"
+    )
+    .then(() => {
+      setLoading(false);
+      alert("Thank you. I will get back to you as soon as possible.");
+
+      setForm({
+        firstname: "",
+        lastname: "",
+        email: "",
+        phonenumber: "",
+        message: "",
+        preferredContact: [],
+        urgency: ""
+      });
+    })
+    .catch((error) => {
+      setLoading(false);
+      console.log(error);
+      alert("Something went wrong!");
+    });
+  };
+
   return (
     <>
     <div className='flex h-screen justify-center pt-20 overflow-auto'>
@@ -13,19 +85,27 @@ const Contact = () => {
         <div className='mx-4'>
           <h1 className='text-secondary text-3xl font-bold mb-3'>Contact Us</h1>
           <p className='text-lg font-medium text-gray-300 mb-4'>Please use this form to get in touch with us. We’ll be happy to assist you with any inquiries or requests.</p>
-          <form className='w-full'>
+          <form className='w-full' onSubmit={handleSubmit}>
             <div className='flex flex-wrap -mx-3 mb-6 md:mb-2'>
               <div className='w-full md:w-1/2 px-3 mb-2'>
                 <label className='font-medium tracking-wide block'>First Name</label>
                 <input
                 className='appearance-none bg-secondary text-gray-800 block border border-gray-700 placeholder-gray-300 rounded-lg w-full p-2.5'
-                placeholder='Mary'/>
+                type="text"
+                name="firstname"
+                placeholder='Mary'
+                value={form.firstname} 
+                onChange={handleChange} />
               </div>
               <div className='w-full md:w-1/2 px-3 mb-2'>
                 <label className='font-medium tracking-wide block'>Last Name</label>
                 <input
                 className='appearance-none bg-secondary text-gray-800 block border border-gray-700 placeholder-gray-300 rounded-lg w-full p-2.5'
-                placeholder='Jane'/>
+                type="text"
+                name="lastname"
+                placeholder='Jane'
+                value={form.lastname}
+                onChange={handleChange}/>
               </div>
             </div>
             <div className='flex flex-wrap -mx-3 mb-6 md:mb-2'>
@@ -33,20 +113,31 @@ const Contact = () => {
                 <label className='font-medium tracking-wide block'>Email Address</label>
                 <input
                 className='appearance-none bg-secondary text-gray-800 block border border-gray-700 placeholder-gray-300 rounded-lg w-full p-2.5'
-                placeholder='someone@gmail.com'/>
+                placeholder='someone@gmail.com'
+                type="email"
+                name="email"
+                value={form.email}
+              onChange={handleChange}/>
               </div>
               <div className='w-full md:w-1/2 px-3 mb-2'>
                 <label className='font-medium tracking-wide block'>Phone Number</label>
                 <input
                 className='appearance-none bg-secondary text-gray-800 block border border-gray-700 placeholder-gray-300 rounded-lg w-full p-2.5'
-                placeholder='0700000000'/>
+                placeholder='0700000000'
+                type="tel"
+                name="phonenumber"
+                value={form.phonenumber}
+                onChange={handleChange}/>
               </div>
             </div>
             <div className='mt-2'>
               <label className=' font-medium block tracking-wide'>Subject of Inquiry</label>
               <input
                 className='appearance-none bg-secondary text-gray-800 block border border-gray-700 placeholder-gray-300 rounded-lg w-full p-2.5'
-                placeholder=''/>
+                placeholder=''
+                name="message"
+                value={form.message}
+              onChange={handleChange}/>
                 <label className='flex flex-col mt-3'>
                   <span className='font-medium mb-4'> Message</span>
                   <textarea
@@ -64,21 +155,26 @@ const Contact = () => {
                 <h1 className='font-medium mb-3'>Preferred Contact</h1>
                 <div className='flex space-x-3'>
                   <input
-                  name='Email'
+                  name='preferredContact'
                   type='checkbox'
+                  value='Email'
+                  checked={form.preferredContact.includes("Email")} onChange={handleChange}
                   className='rounded-full bg-white'/>
                   <label className='font-medium'>Email</label>
                 </div>
                 <div className='flex space-x-3'>
                   <input
-                  name='Email'
+                  value='Phone Call'
+                  checked={form.preferredContact.includes("Phone Call")} onChange={handleChange}
                   type='checkbox'
                   className='rounded-full bg-white'/>
                   <label className='font-medium'>Phone Call</label>
                 </div>
                 <div className='flex space-x-3'>
                   <input
-                  name='Email'
+                  name='preferredContact'
+                  value='Text Message'
+                  checked={form.preferredContact.includes("Text Message")} onChange={handleChange} 
                   type='checkbox'
                   className='rounded-full bg-white'/>
                   <label className='font-medium'>Text Message</label>
@@ -88,24 +184,15 @@ const Contact = () => {
               <div className='mt-5 space-y-2 md:pl-20'>
                 <h1 className='font-medium mb-3'>Urgency of Inquiry</h1>
                 <div className='flex space-x-3'>
-                  <input
-                  name='Email'
-                  type='checkbox'
-                  className='rounded-full bg-white'/>
+                <input type='radio' name='urgency' value='Urgent' checked={form.urgency === "Urgent"} onChange={handleChange} /> 
                   <label className='font-medium'>Urgent</label>
                 </div>
                 <div className='flex space-x-3'>
-                  <input
-                  name='Email'
-                  type='checkbox'
-                  className='rounded-full bg-white'/>
+                <input type='radio' name='urgency' value='Urgent' checked={form.urgency === "Urgent"} onChange={handleChange} /> 
                   <label className='font-medium'>Normal</label>
                 </div>
                 <div className='flex space-x-3'>
-                  <input
-                  name='Email'
-                  type='checkbox'
-                  className='rounded-full bg-white'/>
+                <input type='radio' name='urgency' value='Not Urgent' checked={form.urgency === "Not Urgent"} onChange={handleChange} />
                   <label className='font-medium'>Not Urgent</label>
                 </div>
               </div>
@@ -115,7 +202,7 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary mt-2'
           >
-            {"Send"}
+            {loading ? "Sending..." : "Send"}
           </button>
           </form>
         </div>
